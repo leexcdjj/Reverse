c9db76f74f628f8e6942df758980dcd4e3b8b900
12933 <12933@>
2024-05-19 02:33:47 +0800

    feat: 跳过天龙八部登录器登录游戏

---
+++ CanCanNeedJJBong.Reverse.OpenTLBB/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace CanCanNeedJJBong.Reverse.OpenTLBB;
+
+class Program
+{
+    /// <summary>
+    /// 跳过天龙八部登录器
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Reverse!");
+
+        string tlbbGamePath = "D:\\XTLBBCZ\\Bin\\Game.exe";
+        string arg = "-fl";
+
+        ProcessStartInfo tlbbProcesstInfo = new ProcessStartInfo();
+        tlbbProcesstInfo.FileName = tlbbGamePath;
+        tlbbProcesstInfo.Arguments = arg;
+        
+        // 启动目录设置，因为game.exe要读取dll文件等
+        tlbbProcesstInfo.WorkingDirectory = "D:\\XTLBBCZ\\Bin";
+
+        Process.Start(tlbbProcesstInfo);
+        
+        Console.ReadLine();
+    }
+}
